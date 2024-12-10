a3f05dcaacb98105333b99b18b723131df4c11f8
Karthikgowda143 <karthiknatrajgowda@gmail.com>
2024-12-10 15:58:43 +0530

    fil

---
+++ Core/Inc/EEPROM.h
@@ -0,0 +1,23 @@
+/*
+ * EEPROM.h
+ *
+ *  Created on: Sep 2, 2024
+ *      Author: karth
+ */
+
+#ifndef INC_EEPROM_H_
+#define INC_EEPROM_H_
+
+#include "stdint.h"
+#include "stm32f4xx_hal.h"
+
+
+void EEPROM_Write (uint16_t page, uint16_t offset, uint8_t *data, uint16_t size);
+void EEPROM_Read (uint16_t page, uint16_t offset, uint8_t *data, uint16_t size);
+void EEPROM_PageErase (uint16_t page);
+
+void EEPROM_Write_NUM (uint16_t page, uint16_t offset, float  fdata);
+float EEPROM_Read_NUM (uint16_t page, uint16_t offset);
+
+
+#endif /* INC_EEPROM_H_ */
